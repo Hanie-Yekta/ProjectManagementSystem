aee3ce21dd6eba40f67802b4d4350d446df8dd35
Hanie Yekta <yekta.hnie@gmail.com>
2024-11-23 00:34:47 +0330

    add CRUD operation for task

--- Projects/urls.py
+++ Projects/urls.py
@@ -7,4 +7,6 @@ app_name = 'projects'
 urlpatterns = [
     path('create-list-project/', views.ProjectListCreateView.as_view(), name='create_list_project'),
     path('update-delete-project/<int:pk>/', views.ProjectUpdateDeleteView.as_view(), name='update_delete_project'),
+    path('<int:project_id>/create-list-task/', views.TaskListCreateView.as_view(), name='create_list_task'),
+    path('update-delete-task/<int:pk>/', views.TaskUpdateDeleteView.as_view(), name='update_delete_task'),
 ]
