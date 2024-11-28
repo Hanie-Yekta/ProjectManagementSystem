de7610fc47ae266443391ed2b5de4dcef868669e
Hanie Yekta <yekta.hnie@gmail.com>
2024-11-29 01:59:27 +0330

    added CRUD operation for financial outcome model

---
+++ Financials/serializers.py
@@ -0,0 +1,271 @@
+from rest_framework import serializers
+from abc import ABC
+from django.contrib.contenttypes.models import ContentType
+from django.utils.timezone import now
+
+from .models import FinancialOutcomeRecord, CashPaymentRecord, InstallmentPaymentRecord, CheckPaymentRecord, \
+    InstallmentSchedule
+from Projects.models import Project, Task, SubTask
+from Projects.serializers import ProjectSerializer, TaskSerializer, SubTaskSerializer
+from Accounts.serializers import UserProfileDetailSerializer
+
+
+class FinancialRecordRelationFieldSerializer(serializers.RelatedField, ABC):
+    """
+    Serializes the given related object(Project, Task, SubTask) into its serialized data by its serializers
+    """
+    def to_representation(self, value):
+        """
+        pass the value to related object
+        if object is not among them raise an error.
+        """
+        if isinstance(value, Project):
+            serializer = ProjectSerializer(value)
+        elif isinstance(value, Task):
+            serializer = TaskSerializer(value)
+        elif isinstance(value, SubTask):
+            serializer = SubTaskSerializer(value)
+        else:
+            raise serializers.ValidationError({'Error': 'not excepted model'})
+        return serializer.data
+
+
+class FinancialOutcomeSerializer(serializers.ModelSerializer):
+    """
+    serialize data for financial outcome model.
+    include validation on payment method field.
+    """
+    content_object = FinancialRecordRelationFieldSerializer(read_only=True)
+    created_by = UserProfileDetailSerializer(read_only=True)
+
+    class Meta:
+        model = FinancialOutcomeRecord
+        fields = ('id', 'created_by', 'title', 'description', 'price', 'payment_method', 'content_type', 'object_id',
+                  'content_object')
+        extra_kwargs = {'content_type': {'read_only': True},
+                        'object_id': {'read_only': True},
+                        'content_object': {'read_only': True}}
+
+    def validate(self, attrs):
+        """
+        override this method to validate payment method:
+        Once the payment method value is set, this field cannot be changed
+        """
+        if self.instance:
+            if attrs.get('payment_method'):
+                raise serializers.ValidationError({'Error': 'This field cannot be change.'})
+
+        return attrs
+
+
+    def create(self, validated_data):
+        """
+        override this method to handle generic foreignkey relation and link it to the proper model
+        (fill its fields by given values)
+        base on payment method field, create an instance in proper model (cash, check, installment)
+        """
+        model_name = self.context.get('model')
+        try:
+            content_type = ContentType.objects.get(model=model_name.lower())
+        except ContentType.DoesNotExist:
+            raise serializers.ValidationError({'Error': f'model {model_name} is not valid'})
+
+        validated_data['object_id'] = self.context.get('object_id')
+        validated_data['content_type'] = content_type
+
+        financial_record = FinancialOutcomeRecord.objects.create(**validated_data)
+
+        if financial_record.payment_method == 'cash':
+            CashPaymentRecord.objects.create(financial_outcome=financial_record)
+        elif financial_record.payment_method == 'check':
+            CheckPaymentRecord.objects.create(financial_outcome=financial_record)
+        elif financial_record.payment_method == 'installment':
+            InstallmentPaymentRecord.objects.create(financial_outcome=financial_record)
+
+        return financial_record
+
+
+class CashPaymentSerializer(serializers.ModelSerializer):
+    """
+    serialize data for cash payment model.
+    include validation on payment date field.
+    """
+    financial_outcome = FinancialOutcomeSerializer(read_only=True)
+
+    class Meta:
+        model = CashPaymentRecord
+        fields = ('id', 'payment_date', 'financial_outcome')
+
+    def validate(self, attrs):
+        """
+        override this method to validate payment date -> date must be in today or in future
+                                                        if the field is empty -> required
+        Once the payment date value is set, this field cannot be changed
+        """
+        if self.instance:
+            payment_date = attrs.get('payment_date')
+            cash_obj = self.context['payment_method']
+
+            if payment_date:
+                if cash_obj.payment_date:
+                    raise serializers.ValidationError({'Error': 'Payment date cannot be change'})
+
+                elif payment_date < now().date():
+                    raise serializers.ValidationError({'Error': 'The payment date must be today or in future!'})
+
+            elif payment_date is None and cash_obj.payment_date is None:
+                raise serializers.ValidationError({'Error': 'You must enter a payment date!'})
+
+        return attrs
+
+
+class CheckPaymentSerializer(serializers.ModelSerializer):
+    """
+    serialize data for check payment model.
+    include validation on check date and check number fields.
+    """
+    financial_outcome = FinancialOutcomeSerializer(read_only=True)
+
+    class Meta:
+        model = CheckPaymentRecord
+        fields = ('id', 'check_date', 'check_number', 'financial_outcome')
+
+
+    def validate(self, attrs):
+        """
+        override this method to validate check date -> date must be in today or in future
+                                                        if the field is empty -> required
+                                validate check number -> it must have 16 digits
+                                                         if the field is empty -> required
+        Once these two field values set, values cannot be changed
+        """
+        if self.instance:
+            check_date = attrs.get('check_date')
+            check_number = attrs.get('check_number')
+            check_obj = self.context['payment_method']
+
+            if check_date:
+                if check_obj.check_date:
+                    raise serializers.ValidationError({'Error': "You can't change check date field!"})
+
+                elif check_date < now().date():
+                    raise serializers.ValidationError({'Error': 'The check date must be today or in future!'})
+
+            elif check_date is None and check_obj.check_date is None:
+                raise serializers.ValidationError({'Error': 'You must enter a check date!'})
+
+            if check_number:
+                if check_obj.check_number:
+                    raise serializers.ValidationError({'Error': "You can't change check number field!"})
+
+                elif len(check_number)<16:
+                    raise serializers.ValidationError({'Error': 'The check number must have 16 digits'})
+
+            elif check_number is None and check_obj.check_number is None:
+                raise serializers.ValidationError({'Error': 'You must enter a check number!'})
+
+        return attrs
+
+
+
+class InstallmentPaymentSerializer(serializers.ModelSerializer):
+    """
+    serialize data for installment payment model.
+    include validation on count of installments field.
+    """
+    financial_outcome = FinancialOutcomeSerializer(read_only=True)
+
+    class Meta:
+        model = InstallmentPaymentRecord
+        fields = ('id', 'count_installments', 'financial_outcome')
+
+    def validate(self, attrs):
+        """
+        override this method to validate count installment -> count should be 4 or less
+                                                        if the field is empty -> required
+        Once count installment field value sets, it cannot be changed
+        """
+        if self.instance:
+            count_installments = attrs.get('count_installments')
+            installment_obj = self.context['payment_method']
+
+            if count_installments:
+                if installment_obj.count_installments:
+                    raise serializers.ValidationError({'Error': 'You cant change count installments!'})
+
+                elif count_installments > 4:
+                    raise serializers.ValidationError({'Error': 'The count installments must be less than 4'})
+
+            elif count_installments in None and installment_obj.count_installments is None:
+                raise serializers.ValidationError({'Error': 'You must enter a count installments!'})
+
+        return attrs
+
+    def update(self, instance, validated_data):
+        """
+        override this method to handle update installment payment operation
+        creates records in the installment schedule model based on the count installments field.
+        """
+        obj = super().update(instance, validated_data)
+
+        for i in range(instance.count_installments):
+            InstallmentSchedule.objects.create(installment_id=instance)
+
+        return obj
+
+
+class InstallmentScheduleSerializer(serializers.ModelSerializer):
+    """
+    serialize data for installment schedule model.
+    include validation on date field.
+    """
+    class Meta:
+        model = InstallmentSchedule
+        fields = ('id', 'date', 'installment_status', 'installment_id')
+        extra_kwargs = {'installment_id': {'read_only': True},
+                        'installment_status': {'read_only': True}}
+
+    def validate(self, attrs):
+        """
+        override this method to validate date -> date must be in today or in future
+                                                if the field is empty -> required
+        Once these two field values set, values cannot be changed
+        """
+        if self.instance:
+            date = attrs.get('date')
+            if date:
+                if self.instance.date is not None:
+                    raise serializers.ValidationError({'Error': 'You cant change date of installment!'})
+
+                elif date < now().date():
+                    raise serializers.ValidationError({'Error': 'The installment date must be today or in future!'})
+
+            elif date in None and self.instance.date is None:
+                raise serializers.ValidationError({'Error': 'You must enter date of installment!'})
+
+        return attrs
+
+    def update(self, instance, validated_data):
+        """
+        override this method to handle update installment schedule operation
+        retrieves all installment schedules related to specific installment.
+        check -> input date is not duplicate and doesn't have same month with another instance.
+        """
+        date = validated_data.get('date')
+        installment_obj = self.instance.installment_id
+        installment_schedule_objs = InstallmentSchedule.objects.filter(installment_id=installment_obj)
+
+        for schedule_obj in installment_schedule_objs:
+            if schedule_obj.date is not None:
+                if schedule_obj.date == date:
+                    raise serializers.ValidationError({'Error': 'This date is picked by another installment!'})
+
+                elif schedule_obj.date.month == date.month:
+                    raise serializers.ValidationError({'Error': "You can't set two installments in one month!"})
+
+
+        return super().update(instance, validated_data)
+
+
+
+
--- Financials/views.py
+++ Financials/views.py
@@ -1,3 +1,156 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
 
-# Create your views here.
+from .models import FinancialOutcomeRecord, CashPaymentRecord, CheckPaymentRecord, InstallmentPaymentRecord, \
+    InstallmentSchedule
+from . import serializers
+
+
+class FinancialOutcomeListCreateView(generics.ListCreateAPIView):
+    """
+    this view is used to listing and creating financial outcome records.
+    methods -> GET: for show the list of financial outcome records
+               POST: for create a new financial outcome record
+    permission ->  Only authenticated users
+    """
+    permission_classes = (permissions.IsAuthenticated,)
+    serializer_class = serializers.FinancialOutcomeSerializer
+
+
+    def get_queryset(self):
+        """
+        return user's financial outcome records.
+        """
+        return FinancialOutcomeRecord.objects.filter(created_by=self.request.user)
+
+    def get_serializer_context(self):
+        """
+        sent additional data (object_id, model name) to serializer with context to validate fields properly.
+        """
+        context = super().get_serializer_context()
+
+        context['object_id'] = self.kwargs['object_id']
+        context['model'] = self.kwargs['model']
+
+        return context
+
+    def perform_create(self, serializer):
+        """
+        pass the user to serializer as creator of financial outcome record.
+        save financial outcome record's information
+        """
+        serializer.is_valid(raise_exception=True)
+        serializer.save(created_by=self.request.user)
+
+
+
+class FinancialOutcomeUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    this view is used to update and delete a financial outcome record.
+    methods -> PUT, PATCH: for update the information of the financial outcome record
+                DELETE: for delete the financial outcome record
+    permission ->  Only authenticated users
+    """
+    permission_classes = (permissions.IsAuthenticated,)
+    serializer_class = serializers.FinancialOutcomeSerializer
+    queryset = FinancialOutcomeRecord
+
+    def perform_update(self, serializer):
+        """
+        validate and update the financial outcome record data
+        """
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+    def perform_destroy(self, instance):
+        """
+        delete the financial outcome record
+        """
+        instance.delete()
+
+
+class PaymentMethodUpdateView(generics.RetrieveUpdateAPIView):
+    """
+    this view is used to update a payment method instance of financial outcome record.
+    methods -> PUT, PATCH: for update the information of the payment method
+    permission ->  Only authenticated users
+    """
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self):
+        """
+        retrieves the payment method instance based on the associated financial outcome record.
+        """
+        financial_obj = FinancialOutcomeRecord.objects.get(id=self.kwargs['financial_id'])
+
+        if financial_obj.payment_method == 'cash':
+            return CashPaymentRecord.objects.get(financial_outcome=financial_obj)
+        elif financial_obj.payment_method == 'check':
+            return CheckPaymentRecord.objects.get(financial_outcome=financial_obj)
+        elif financial_obj.payment_method == 'installment':
+            return InstallmentPaymentRecord.objects.get(financial_outcome=financial_obj)
+
+
+    def get_serializer_class(self):
+        """
+        selects the appropriate serializer based on the payment method.
+        """
+        financial_obj = FinancialOutcomeRecord.objects.get(id=self.kwargs['financial_id'])
+
+        if financial_obj.payment_method == 'cash':
+            return serializers.CashPaymentSerializer
+        elif financial_obj.payment_method == 'check':
+            return serializers.CheckPaymentSerializer
+        elif financial_obj.payment_method == 'installment':
+            return serializers.InstallmentPaymentSerializer
+
+    def get_serializer_context(self):
+        """
+        sent additional data (payment_method) to serializer with context to validate fields properly.
+        """
+        context = super().get_serializer_context()
+        context['payment_method'] = self.get_object()
+        return context
+
+
+    def perform_update(self, serializer):
+        """
+        validate and update the payment method instance data
+        """
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+
+class InstallmentScheduleListView(generics.ListAPIView):
+    """
+    this view is used to listing installment schedules for a specific installment payment.
+    methods -> GET: for show the list of installment schedules
+    permission ->  Only authenticated users
+    """
+    permission_classes = (permissions.IsAuthenticated,)
+    serializer_class = serializers.InstallmentScheduleSerializer
+
+    def get_queryset(self):
+        """
+        return installment payment's installment schedule records
+        """
+        installment_obj = InstallmentPaymentRecord.objects.get(id=self.kwargs['installment_id'])
+        return InstallmentSchedule.objects.filter(installment_id=installment_obj)
+
+
+
+class InstallmentScheduleUpdateView(generics.RetrieveUpdateAPIView):
+    """
+    this view is used to update an installment schedule instance.
+    methods -> PUT, PATCH: for update the information of the installment schedule record
+    permission ->  Only authenticated users
+    """
+    permission_classes = (permissions.IsAuthenticated,)
+    serializer_class = serializers.InstallmentScheduleSerializer
+    queryset = InstallmentSchedule
+
+    def perform_update(self, serializer):
+        """
+        validate and update the installment schedule record data
+        """
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
