19808fb5e1b58a96c51c34342f93ae10d3f4bf58
Hanie Yekta <yekta.hnie@gmail.com>
2024-12-02 04:47:09 +0330

    swagger added

--- ProjectManagement/urls.py
+++ ProjectManagement/urls.py
@@ -15,15 +15,32 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Project Management API",
+      default_version='v1',
+      description="this is a api version of projectmanagement.com website",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="yekta.hnie@gmail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('Accounts.urls')),
     path('projects/', include('Projects.urls')),
     path('financials/', include('Financials.urls')),
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
 ]
 
